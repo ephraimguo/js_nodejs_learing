7ba9af9d66cd160b516d2b0ea74ebe94dbf125bd
ephraimguo <ephraimguo@gmail.com>
2018-01-22 23:27:51 +0800

    1st push from imac home

--- wk2/obj_copy/symbol_privateAttr.js
+++ wk2/obj_copy/symbol_privateAttr.js
@@ -1,16 +1,35 @@
-const o = factory();
-// o.change("oookkk");
-console.log(o.key1);
+const obj = factory();
+// obj.change("oookkk");
+console.log(obj);
 
 
 function factory(){
     const key1 = Symbol();
 
     var obj = {
-        key1 : 122,
+        [key1]:122,
         change(value){
-            this.key1 = value;
+            this[key1] = value;
         }
     };
-    return o;
+    return obj;
 }
+
+// const o = factory();
+// o.change("oookkk");
+// console.log(o);
+//
+// function factory(){
+//
+//     const key1 = Symbol(); // symbol
+//
+//     var o = {
+//         [key1]:122,
+//         change(value){
+//             this[key1] = value;
+//         }
+//     }
+//
+//     return o;
+//
+// }
