7e678b864e3e2e7d21bd5a264f375933536ee08e
ephraimguo <ephraimguo@gmail.com>
2018-01-24 15:27:30 +0800

    RegExp homework done

---
+++ wk3/homework/1RegExp.js
@@ -0,0 +1,22 @@
+
+var str = "fjskfj192.168.1.101sl fjkdsjfkls fjklsfjlsd127.0.0.1fjksldfjafjio jfk192.168.1.101sljfs function fsdjfkldsfjsklfsklffjoijfoiaf asfjs fed jfjf jf sdjfsi jfi jafis function dd ( ) { }afjsdiajf iosa function abc(){} jfisjad fjsaiofjaoissf addedfds";
+
+var re_ip = /(\d{1,3}\.){3}\d{1,3}/g;
+var res_ip_exec;
+
+do {
+    res_ip_exec = re_ip.exec(str);
+    console.log(res_ip_exec && res_ip_exec[0]);
+}
+while(res_ip_exec);
+
+console.log("----------");
+
+var re_fun = /function\s\w+\s+?\(\s*?\)\s*?{\s*?}/g;
+var res_fun_exec;
+
+do {
+    res_fun_exec = re_fun.exec(str);
+    console.log(res_fun_exec && res_fun_exec[0]);
+    // console.log(res_fun_exec);
+}while(res_fun_exec);
